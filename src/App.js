9860f01de8b11d552821a47e71f34ae16debbe43
choijaewoo <cchoi00332244@gmail.com>
2024-03-26 16:16:25 +0900

    start frontend with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import findmeLogo from './image/findme_logo.png'
 import './App.css';
 
 function App() {
+
+  let posts = "cjw";
+
+  function testFunct(input){
+    return input+1;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="black-nav">
+
+        <div className="logoimg">
+        </div>
+
+        <div>
+          FindME
+        </div>
+      
+        
+      </div>
+
+      <h4>
+        {posts}
+      </h4>
+
+      <h4>
+        {testFunct(3)}
+      </h4>
+      
+
+
+
     </div>
   );
 }
