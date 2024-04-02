9f1522330dcd0bf7432dafa97db7b4f30c080410
choijaewoo <cchoi00332244@gmail.com>
2024-04-02 21:07:04 +0900

    error fix

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import MainPage from './component/MainPage';
 import TestPage from './component/TestPage';
 import Find from './component/Find'
-import Lost from './component/Lost'
 import Login from './component/Login';
 import Search from './component/Search';
 import ApiTest from './component/ApiTest';
+import Lost from './component/Lost';
 
 function App() {
   return (
@@ -22,16 +22,16 @@ function App() {
           <Route path="/find">
             <Find />
           </Route>
-          <Route path="/lost">
-            <Lost />
-          </Route>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/search">
             <Search/>
           </Route>
-          <Route path="/api-test">
+          <Route path="/lost">
+            <Lost/>
+          </Route>
+          <Route path="/api/test">
             <ApiTest/>
           </Route>
         </Switch>
